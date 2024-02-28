780e3fdf56e194b1a5ff5862578697dd10708c1c
hlibk <hlibk@proton.me>
2024-02-28 15:24:56 -0600

    Initial template setting up .htaccess, cdn libraries, and stylesheet

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand logo text-light" href="index">
+            Collecti<span class="fw-bold">Byte</span>
+        </a>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    $ref = "./";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="CollectiByte"/>
+        <meta name="author" content="korhlibri"/>
+        <title>CollectiByte</title>
+
+        <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue@3.4.21/dist/vue.global.min.js"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <link href="<?php echo $ref?>assets/css/style.css" rel="stylesheet">
+    </head>
+    <body class="bg-dark">
+        <?php
+            require_once("header.php");
+        ?>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+        <h1 class="text-light">Hello World<br><br><br></h1>
+    </body>
+</html>
