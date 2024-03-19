7b405ac8b2d51e4b28e67ba4599dbc2718caa3d3
hlibk <hlibk@proton.me>
2024-03-18 18:16:50 -0600

    Add SQL queries for articles and products

---
+++ tables.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE collectibyte;
+
+CREATE TABLE collectibyte.articles(
+    id int(9) NOT NULL AUTO_INCREMENT, 
+    permalink varchar(25) NOT NULL, 
+    creation_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    archived int(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+) COLLATE 'utf8_general_ci';
+
+CREATE TABLE collectibyte.products(
+    id int(9) NOT NULL AUTO_INCREMENT,
+    permalink varchar(25) NOT NULL,
+    amount int(9) NOT NULL,
+    price decimal(10,2) NOT NULL,
+    creation_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    archived int(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+) COLLATE 'utf8_general_ci';
