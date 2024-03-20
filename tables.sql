6d96de6ef2710ebe622633e526648629d18e8e78
hlibk <hlibk@proton.me>
2024-03-19 22:00:48 -0600

    Add title and description to tables, and create new table for article text

--- tables.sql
+++ tables.sql
@@ -3,6 +3,8 @@ CREATE DATABASE collectibyte;
 CREATE TABLE collectibyte.articles(
     id int(9) NOT NULL AUTO_INCREMENT, 
     permalink varchar(25) NOT NULL UNIQUE, 
+    title varchar(50) NOT NULL UNIQUE,
+    description text NOT NULL,
     creation_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
     archived int(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
@@ -11,6 +13,8 @@ CREATE TABLE collectibyte.articles(
 CREATE TABLE collectibyte.products(
     id int(9) NOT NULL AUTO_INCREMENT,
     permalink varchar(25) NOT NULL UNIQUE,
+    title varchar(50) NOT NULL UNIQUE,
+    description text NOT NULL,
     amount int(9) NOT NULL,
     price decimal(10,2) NOT NULL,
     creation_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
