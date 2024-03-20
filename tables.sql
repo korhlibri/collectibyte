1268ea8072feda71e01261ae86881ac8288130bd
hlibk <hlibk@proton.me>
2024-03-19 21:41:50 -0600

    Add unique constraints to tables

--- tables.sql
+++ tables.sql
@@ -2,7 +2,7 @@ CREATE DATABASE collectibyte;
 
 CREATE TABLE collectibyte.articles(
     id int(9) NOT NULL AUTO_INCREMENT, 
-    permalink varchar(25) NOT NULL, 
+    permalink varchar(25) NOT NULL UNIQUE, 
     creation_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
     archived int(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
@@ -10,7 +10,7 @@ CREATE TABLE collectibyte.articles(
 
 CREATE TABLE collectibyte.products(
     id int(9) NOT NULL AUTO_INCREMENT,
-    permalink varchar(25) NOT NULL,
+    permalink varchar(25) NOT NULL UNIQUE,
     amount int(9) NOT NULL,
     price decimal(10,2) NOT NULL,
     creation_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
